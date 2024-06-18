f5e26519f4981dddafce7852781ff2c37f3edec0
shubam rajput <113655062+SHUBHAMRAJPUT9665@users.noreply.github.com>
2024-06-18 16:31:43 +0530

    Add react redux config store

---
+++ client/src/Redux/store.js
@@ -0,0 +1,13 @@
+import {configureStore} from '@reduxjs/toolkit'
+import AuthSliceReducer from './Slices/AuthSlice'
+
+
+const store = configureStore({
+    reducer:{
+        auth:AuthSliceReducer
+    },
+    devTools:true
+})
+
+
+export default store
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+//component imports
+import React from "react";
+import "./index.css";
+import { BrowserRouter } from "react-router-dom";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import { Toaster } from "react-hot-toast";
+import { Provider } from "react-redux";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <Provider>
+    <BrowserRouter>
+      <App />
+      <Toaster />
+    </BrowserRouter>
+  </Provider>
+);
