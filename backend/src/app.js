f1b22d0580bd02df0beef1f51dada80dc634b013
shubham rajput <113655062+SHUBHAMRAJPUT9665@users.noreply.github.com>
2024-07-24 05:28:50 +0530

    Update app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -14,7 +14,7 @@ app.use((req, res, next) => {
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   next();
 });
-
+app.set("trust proxy", 1);
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true, limit: "20kb" }));
