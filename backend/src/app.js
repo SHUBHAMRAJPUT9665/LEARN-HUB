f5ba1ec11285f028e9db0fd805ff2c4f27581a5b
shubam rajput <113655062+SHUBHAMRAJPUT9665@users.noreply.github.com>
2024-07-23 07:48:49 +0530

    env

--- backend/src/app.js
+++ backend/src/app.js
@@ -7,29 +7,16 @@ import courseRoute from "./routes/course.route.js";
 import paymentRoute from './routes/payment.route.js'
 const app = express();
 
-app.use(express.json());
 
 const corsOptions = {
-<<<<<<< HEAD
   origin: process.env.FRONTEND_URL, // Adjust this based on your environment
   credentials: true,  // Allow credentials (cookies, authorization headers, etc.)
-=======
-  origin: [process.env.FRONTEND_URL],  // Specific origin
-  credentials: true  // Allow credentials (cookies, authorization headers, etc.)
->>>>>>> 2d96a313dad9bf421196395fc953faca3ac17766
 };
 
 app.use(cors(corsOptions));
+
 app.use(cookieParser());
-
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', process.env.FRONTEND_URL);  // Specific origin
-  res.header('Access-Control-Allow-Credentials', 'true');
-  res.header('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
-  next();
-});
-
+app.use(express.json());
 app.use(express.urlencoded({ extended: true, limit: "20kb" }));
 app.use(express.static("public"));
 app.use(morgan("dev"));
