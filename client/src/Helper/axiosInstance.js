4234846e3434671af0c13382e29076bb8682daa5
shubham rajput <113655062+SHUBHAMRAJPUT9665@users.noreply.github.com>
2024-07-24 05:53:08 +0530

    Update axiosInstance.js

--- client/src/Helper/axiosInstance.js
+++ client/src/Helper/axiosInstance.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-
+axios.defaults.withCredentials = true;
 // const BASE_URL = 'https://learn-hub-backend.onrender.com/api/v1/';
 const BASE_URL = 'http://localhost:5000/api/v1/';
 
