cbd8db18ccd069bbaff3ff61320f392a6c2b700b
shubam rajput <113655062+SHUBHAMRAJPUT9665@users.noreply.github.com>
2024-07-24 06:15:10 +0530

    axios

--- client/src/Helper/axiosInstance.js
+++ client/src/Helper/axiosInstance.js
@@ -1,9 +1,6 @@
 import axios from 'axios';
-<<<<<<< HEAD
 
-=======
 axios.defaults.withCredentials = true;
->>>>>>> 4234846e3434671af0c13382e29076bb8682daa5
 // const BASE_URL = 'https://learn-hub-backend.onrender.com/api/v1/';
 const BASE_URL = 'http://localhost:5000/api/v1/';
 
